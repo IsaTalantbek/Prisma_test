c65f7cd51ce3e35374a2143a32d00cf983170a53
IsaT <talantbekisa778@gmail.com>
2024-12-22 10:27:26 +0600

    update-1.3.1

--- dist/routes/apiRouter.js
+++ dist/routes/apiRouter.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import authRouter from './authRouter.js'
-import DbRouter from './dbRouter'
+import DbRouter from './dbRouter.js'
 import { authenticateToken, checkRole } from '../middleware/authMiddleware'
 const router = Router()
 router.use('/db', authenticateToken, checkRole('admin'), DbRouter)
